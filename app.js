f2b7576d2e2f5d91ce19634e6b82d1774ff55b35
fersan <fst-96@hotmail.com>
2021-04-09 10:18:44 -0500

    Añadiendo las clases de Tarea y Tareas

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 require('colors');
+const { v4: uuidv4 } = require('uuid');
 const { inquirerMenu,pausa } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas')
 
-console.clear();
+
 
 const main = async ( ) => {
 
     console.clear();
     let opt = '';
+
     do{
 
         opt = await inquirerMenu();
 
         console.log({opt});
+
+        // const tarea = new Tarea('Comparar comida');
+
+        // const tarea = new Tarea('Descripcion de la tarea');
+        // const tareas = new Tareas();
+
+        // Asignando tarea.id en tareas.listado
+        // tareas._listado[tarea.id] = tarea;
+        // console.log(tareas);
 
         await pausa();
 
@@ -21,3 +34,4 @@ const main = async ( ) => {
 
 
 main();
+
